ca27a31bf089523ae1ab13c96c23823717901eff
Christine Romano <romano.christine@gmail.com>
2025-08-15 21:55:42 -0400

    Completed Assignment2

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -1,49 +1,94 @@
 /* ASSIGNMENT 2 */
 /* SECTION 2 */
+/* Christine Romano */
 
 -- COALESCE
 /* 1. Our favourite manager wants a detailed long list of products, but is afraid of tables! 
 We tell them, no problem! We can produce a list with all of the appropriate details. 
 
-Using the following syntax you create our super cool and not at all needy manager a list:
+Using the following syntax you create our super cool and not at all needy manager a list: */
 
 SELECT 
-product_name || ', ' || product_size|| ' (' || product_qty_type || ')'
-FROM product
+product_name || ', ' || product_size|| ' (' || product_qty_type || ')' as list_for_manager
+FROM product;
 
-But wait! The product table has some bad data (a few NULL values). 
+/* But wait! The product table has some bad data (a few NULL values). 
 Find the NULLs and then using COALESCE, replace the NULL with a 
 blank for the first problem, and 'unit' for the second problem. 
 
-HINT: keep the syntax the same, but edited the correct components with the string. 
+/* Finding nulls ; testing IFNULL*/
+
+SELECT *
+,IFNULL(product_size,'') as product_size_no_null
+,IFNULL (product_qty_type, 'unit') as qty_type_no_null
+FROM product;
+
+/* Finding nulls ; testing COALESCE*/
+
+SELECT *
+,COALESCE(product_size,'') as product_size_no_null
+,COALESCE(product_qty_type, 'unit') as qty_type_no_null
+FROM product;
+
+
+/*HINT: keep the syntax the same, but edited the correct components with the string. 
 The `||` values concatenate the columns into strings. 
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
 
-
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
 visits to the farmer’s market (labeling each market date with a different number). 
-Each customer’s first visit is labeled 1, second visit is labeled 2, etc. 
+Each customer’s first visit is labeled 1, second visit is labeled 2, etc. */
 
-You can either display all rows in the customer_purchases table, with the counter changing on
+
+/*SELECT *,
+ROW_NUMBER () OVER (PARTITION BY customer_id ORDER BY customer_id) as visit_number
+FROM
+	(SELECT DISTINCT
+	customer_id,
+	market_date
+	FROM customer_purchases
+	ORDER BY customer_id, market_date); */ 
+	
+SELECT DISTINCT
+    customer_id,
+    market_date,
+    dense_rank () OVER (PARTITION BY customer_id ORDER BY market_date) AS visit_number
+FROM customer_purchases;
+
+/*You can either display all rows in the customer_purchases table, with the counter changing on
 each new market date for each customer, or select only the unique market dates per customer 
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
-
 
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+SELECT * FROM
+	(SELECT DISTINCT
+		customer_id,
+		market_date,
+		dense_rank () OVER (PARTITION BY customer_id ORDER BY market_date DESC) AS visit_number
+	FROM customer_purchases)
+WHERE visit_number = 1;
 
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
 
+/*SELECT *
+	,row_number () OVER (Partition by product_id order by customer_id) as purchase_tally
+	FROM customer_purchases
+	ORDER BY customer_id*/
 
+	SELECT *
+	,COUNT () OVER (Partition by product_id order by customer_id) as purchase_tally
+	FROM customer_purchases
+	ORDER BY customer_id;
 
 -- String manipulations
 /* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
@@ -73,8 +118,28 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
+DROP TABLE IF EXISTS temp.daily_total;
 
-
+CREATE TABLE temp.daily_total AS
+	SELECT 
+		market_date
+		,ROUND(SUM(quantity*cost_to_customer_per_qty),2) as total_cost
+	FROM customer_purchases
+		GROUP BY market_date 
+		ORDER BY total_cost ASC;		
+	
+SELECT
+		market_date
+		,max (total_cost) as earnings
+	FROM temp.daily_total
+	
+	UNION 
+	
+SELECT 
+		market_date
+		,min (total_cost) as earnings
+	FROM temp.daily_total;
+	
 
 /* SECTION 3 */
 
@@ -89,7 +154,21 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
-
+SELECT
+	vendor_id, vendor_name, product_id, product_name, price_of_five, price_of_five * 26 as sum_price
+	,SUM(price_of_five * 26) OVER(PARTITION BY vendor_id) as earnings_per_vendor
+		FROM
+			(SELECT *
+			FROM 
+				(SELECT vi.vendor_id, vendor_name, vi.product_id, product_name, original_price*5 as price_of_five
+				FROM vendor_inventory as vi
+				INNER JOIN product as p
+				ON vi.product_id = p.product_id
+				INNER JOIN vendor as v
+				ON vi.vendor_id = v.vendor_id
+				GROUP BY vi.product_id)
+			CROSS JOIN customer)
+GROUP BY product_id;
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -97,35 +176,67 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS product_units;
+
+CREATE TABLE  product_units  as 
+SELECT 
+*,
+CURRENT_TIMESTAMP as snapshot_timestamp
+FROM product
+WHERE product_qty_type = 'unit';
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
+INSERT INTO product_units (product_id, product_name, product_size, product_category_id, product_qty_type, snapshot_timestamp)
+VALUES (24, 'Popsicles', 'each', 3, 'unit', CURRENT_TIMESTAMP);
 
 
 -- DELETE
-/* 1. Delete the older record for the whatever product you added. 
+/* 1. Delete the older record for the whatever product you added. */
 
-HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
+DELETE FROM product_units 
+WHERE product_name = 'Popsicles';
 
+/*HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
-First, add a new column, current_quantity to the table using the following syntax.
+First, add a new column, current_quantity to the table using the following syntax. */
 
 ALTER TABLE product_units
 ADD current_quantity INT;
 
-Then, using UPDATE, change the current_quantity equal to the last quantity value from the vendor_inventory details.
+/* Then, using UPDATE, change the current_quantity equal to the last quantity value from the vendor_inventory details. 
 
 HINT: This one is pretty hard. 
-First, determine how to get the "last" quantity per product. 
-Second, coalesce null values to 0 (if you don't have null values, figure out how to rearrange your query so you do.) 
-Third, SET current_quantity = (...your select statement...), remembering that WHERE can only accommodate one column. 
-Finally, make sure you have a WHERE statement to update the right row, 
-	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
+First, determine how to get the "last" quantity per product. */ 
+
+DROP TABLE IF EXISTS temp.recent_date;
+CREATE TEMP TABLE recent_date AS
+
+SELECT *, coalesce(column_of_nulls, 0) as column_of_zeros
+FROM 
+  (SELECT * FROM
+		(SELECT * 
+			,row_number () OVER (partition by product_id order by market_date desc) as date_count_backwards
+			,(NULL) as column_of_nulls
+			FROM 
+		vendor_inventory)
+	WHERE date_count_backwards = 1);
+	
+UPDATE product_units
+SET current_quantity =  (SELECT quantity FROM recent_date WHERE recent_date.product_id = product_units.product_id);
+
+
+/*Second, coalesce null values to 0 (if you don't have null values, figure out how to rearrange your query so you do.) */
+
+/* Third, SET current_quantity = (...your select statement...), remembering that WHERE can only accommodate one column. 
+Finally, make sure you have a WHERE statement to update the right row, */
+
+	/*you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
 
